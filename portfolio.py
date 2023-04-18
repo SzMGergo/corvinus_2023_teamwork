2c09dae3b7c4b394f339658dfca8ea7cd2a1b763
Gergo53 <sz.m.gergo53@gmail.com>
2023-04-18 11:15:28 +0200

    portfolio return function

--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import utils as u
 
 class Portfolio():
 
@@ -5,10 +7,14 @@ class Portfolio():
         self.d_weights = d_weights
         self.pf_value = pf_value
 
-    # parameters: from date to date
-    def get_pf_returns(self):
+    
 
-        pass
+    # parameters: from date to date
+    def get_pf_returns(self, from_date, to_date):
+        df_joined = u.get_joined_returns(self.d_weights, from_date, to_date)
+        df_weighted_returns = df_joined * pd.Series(self.d_weights)
+        s_portfolio_return = df_weighted_returns.sum(axis=1)
+        return pd.DataFrame(s_portfolio_return, columns=['pf'])
 
     # parameters: from date to date
     def get_returns_of_etfs(self):
--- usage.py
+++ usage.py
@@ -6,5 +6,6 @@ p1 = p.Portfolio(d_weights, pf_value)
 print(p1.d_weights)
 from_date = '2020-03-01'
 to_date = '2021-03-01'
+
 print(p1.get_pf_returns(from_date, to_date))
 print(p1.get_returns_of_etfs(from_date, to_date))
