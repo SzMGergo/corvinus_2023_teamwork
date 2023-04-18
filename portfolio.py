bc060e10be4b52bd6f8cde05fd709f07cb4e8ef9
ntamas <tnagyphd@gmail.com>
2023-04-18 10:58:30 +0200

    new functions

--- portfolio.py
+++ portfolio.py
@@ -4,3 +4,17 @@ class Portfolio():
     def __init__(self, d_weights, pf_value):
         self.d_weights = d_weights
         self.pf_value = pf_value
+
+    # parameters: from date to date
+    def get_pf_returns(self):
+
+        pass
+
+    # parameters: from date to date
+    def get_returns_of_etfs(self):
+
+        pass
+
+
+    def calculate_var(self):
+        pass
--- usage.py
+++ usage.py
@@ -4,3 +4,7 @@ d_weights = {'IEI': 0.6, 'VOO': 0.4}
 pf_value = 100000
 p1 = p.Portfolio(d_weights, pf_value)
 print(p1.d_weights)
+from_date = '2020-03-01'
+to_date = '2021-03-01'
+print(p1.get_pf_returns(from_date, to_date))
+print(p1.get_returns_of_etfs(from_date, to_date))
